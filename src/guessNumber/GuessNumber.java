943400033dfc05731ad1e639ef2f02152fd54e6f
laofoye <cling.lydia@gmail.com>
2012-10-28 16:50:03 +0800

    add a new file

--- src/guessNumber/GuessNumber.java
+++ src/guessNumber/GuessNumber.java
@@ -9,9 +9,13 @@ public class GuessNumber {
     }
 
     public String validate(String userInput) {
-        if(userInput.equals(randomNumber)){
-            return "4a0b";
-        }
-        return "3a0b";
+        int x=0,y=0;
+        char A[],B[];
+        for (int i=0;i<4;i++){
+        for (int j=0;j<4;j++){    if(i=j&A[i]==B[j]) x++;
+            else if (i<j&A[i]==B[j]|i>j&A[i]==B[j])  y++;
+
+     }
+           return x"a"y"b";
     }
 }
